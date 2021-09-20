748fed92022b5da6c42b8bfc052c6edb948c140a
Jaap Coomans <jaapcoomans@gmail.com>
2021-09-20 22:47:24 +0200

    Add toString() to Timeslot

--- src/main/java/nl/jaapcoomans/demo/testdata/conference/domain/TimeSlot.java
+++ src/main/java/nl/jaapcoomans/demo/testdata/conference/domain/TimeSlot.java
@@ -3,9 +3,20 @@ package nl.jaapcoomans.demo.testdata.conference.domain;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public record TimeSlot(LocalDate date, LocalTime startTime, LocalTime endTime) {
     public int lengthInMinutes() {
-        return (int)Duration.between(startTime, endTime).toMinutes();
+        return (int) Duration.between(startTime, endTime).toMinutes();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + date.format(DateTimeFormatter.ISO_DATE)
+                + " "
+                + startTime.format(DateTimeFormatter.ISO_LOCAL_TIME)
+                + "-"
+                + endTime.format(DateTimeFormatter.ISO_LOCAL_TIME)
+                + "]";
     }
 }
