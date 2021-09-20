cbfab9f7f80fbb04e311ded38bea72640d1bafaa
Jaap Coomans <jaapcoomans@gmail.com>
2021-09-20 23:10:01 +0200

    Add validations in Paper

--- src/main/java/nl/jaapcoomans/demo/testdata/conference/domain/Paper.java
+++ src/main/java/nl/jaapcoomans/demo/testdata/conference/domain/Paper.java
@@ -42,11 +42,58 @@ public class Paper {
     }
 
     public void submit() {
-        //TODO: Do validations
+        if (status != Status.DRAFT) {
+            throw new IllegalStateException("Paper can only be submitted if it has status DRAFT");
+        }
+        validateTitle();
+        validateAbstract();
+        validateOutline();
+        validateMessageToProgramCommittee();
         this.status = Status.SUBMITTED;
     }
 
+    private void validateTitle() {
+        if (title == null || title.isBlank()) {
+            throw new IllegalStateException("Title must be filled.");
+        }
+    }
+
+    private void validateAbstract() {
+        if (sessionAbstract == null || sessionAbstract.isBlank()) {
+            throw new IllegalStateException("Abstract must be filled.");
+        }
+    }
+
+    private void validateOutline() {
+        if (outline == null || outline.isBlank()) {
+            throw new IllegalStateException("Outline must be filled.");
+        }
+    }
+
+    private void validateMessageToProgramCommittee() {
+        if (messageToProgramCommittee == null || messageToProgramCommittee.isBlank()) {
+            throw new IllegalStateException("Message to program committee must be filled.");
+        }
+    }
+
+    public void accept() {
+        if (status != Status.SUBMITTED) {
+            throw new IllegalStateException("Paper can only be accepted if it has status SUBMITTED");
+        }
+        this.status = Status.ACCEPTED;
+    }
+
+    public void reject() {
+        if (status != Status.SUBMITTED) {
+            throw new IllegalStateException("Paper can only be rejected if it has status SUBMITTED");
+        }
+        this.status = Status.REJECTED;
+    }
+
     public Session confirm() {
+        if (status != Status.ACCEPTED) {
+            throw new IllegalStateException("Paper can only be confirmed if it has status ACCEPTED");
+        }
         this.status = Status.CONFIRMED;
         return Session.create(
                 this.id.conferenceId,
